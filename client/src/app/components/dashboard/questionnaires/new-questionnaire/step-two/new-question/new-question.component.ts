6ae71de07ae1c7f7413672c0fc193dddd5b00cd2
cesarposas <cesarsarrion@gmail.com>
2021-03-07 04:14:31 +0100

    cambios

--- client/src/app/components/dashboard/questionnaires/new-questionnaire/step-two/new-question/new-question.component.ts
+++ client/src/app/components/dashboard/questionnaires/new-questionnaire/step-two/new-question/new-question.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//Form array es para los formularios dinamicos
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Question } from 'src/app/models/question';
 
 @Component({
   selector: 'app-new-question',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-question.component.css']
 })
 export class NewQuestionComponent implements OnInit {
+  newQuestion: FormGroup;
+  question: Question;
+  correctAnswer= 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService){
+    this.newQuestion = this.fb.group({
+      title: ['', Validators.required],
+      //array de formgroups
+      answers: this.fb.array([])
+    })
   }
 
+  ngOnInit() { }
+
+  //Getter de la clase. newQuestion.Answers
+  get getAnswers(): FormArray{
+    return this.newQuestion.get('answers') as FormArray;
+  }
+
+  addAnswer(): void{
+    this.getAnswers.push(this.fb.group({
+      description: ['', Validators.required],
+      isCorrect: 0
+    }))
+  }
+
+  deleteAnswer(i: number): void{
+    this.getAnswers.removeAt(i);
+  }
 }
