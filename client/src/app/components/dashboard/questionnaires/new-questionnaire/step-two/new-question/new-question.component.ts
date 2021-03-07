7e2043e595024df516561146c7a98379f7aa2959
cesarposas <cesarsarrion@gmail.com>
2021-03-07 18:08:31 +0100

    crear objeto question a partir del formulario

--- client/src/app/components/dashboard/questionnaires/new-questionnaire/step-two/new-question/new-question.component.ts
+++ client/src/app/components/dashboard/questionnaires/new-questionnaire/step-two/new-question/new-question.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 //Form array es para los formularios dinamicos
 import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { Console } from 'console';
 import { ToastrService } from 'ngx-toastr';
+import { Answer } from 'src/app/models/answer';
 import { Question } from 'src/app/models/question';
 
 @Component({
@@ -24,7 +26,22 @@ export class NewQuestionComponent implements OnInit {
     })
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.addDefaultAnswers()
+  }
+
+   addDefaultAnswers(): void{
+    this.addAnswer();
+    this.addAnswer();
+   }
+
+   //cuando se envia el formulario se resetea el formulario
+   //tambien reseteamos el el array de formularios  de respuestas
+   reset(): void {
+     this.newQuestion.reset();
+     this.getAnswers.clear();
+     this.addDefaultAnswers();
+   }
 
   //Getter de la clase. newQuestion.Answers
   get getAnswers(): FormArray{
@@ -39,6 +56,45 @@ export class NewQuestionComponent implements OnInit {
   }
 
   deleteAnswer(i: number): void{
-    this.getAnswers.removeAt(i);
+    if(this.getAnswers.length == 2){
+      this.toastr.error('Debe tener 2 respuestas como');
+    }
+    else{
+      this.getAnswers.removeAt(i);
+    }
+  }
+
+  setCorrectAnswer(index: number): void{
+    this.correctAnswer = index;
+  }
+
+  addQuestion(): void{
+    //obtenemos el titulo de la pregunta del formulario
+    const answerDescription = this.newQuestion.get('title').value;
+
+    //obtenemos el  array de respuestas del formulario
+    const questionAnswers = this.newQuestion.get('answers').value;
+
+    //Creamos un array de respuestas
+    const arrayAnswers: Answer[] = [];
+    
+    //creamos un array de a partir de las respuestas del formulario para crear el objeto Question
+    //foreach con index
+    questionAnswers.forEach((element, index) => {
+      let id = index;
+      let correct: boolean = (index === element.isCorrect);
+      console.log(correct);
+      const answer: Answer = new Answer(element.description, correct, id);
+
+      arrayAnswers.push(answer);
+    });
+
+    //Creamos el objeto question
+    const question: Question = new Question(answerDescription, arrayAnswers);
+
+    console.log(this.correctAnswer);
+    console.log(question);
+
+    this.reset();
   }
 }
